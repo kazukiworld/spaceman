f756b6e3007646cbc740385cc0231d0668598b83
Kazukiworld <kazukistartupsforall@gmail.com>
2020-12-14 17:43:45 -1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {React} from "react";
+import "./App.css";
+import {Canvas} from 'react-three-fiber';
+import {OrbitControls, useGLTFLoader} from 'drei';
+import Background from './space3.jpg';
+
+
+
 
 function App() {
+  const gltf = useGLTFLoader('/spaceman.gltf', true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="canvas" style={{backgroundImage:`url(${Background})`}}>
+          <Canvas 
+          colorManagement
+          camera={{position: [0,0,6], fov: 80}}>
+            <ambientLight intensity={1}/>  
+            <directionalLight intensity ={1} position={[0,10,10]}/>
+            <pointLight intensity={1} position={[0,10,10]}/>
+            <pointLight intensity={1} position={[0,10,-10]}/>
+            <pointLight intensity={1} position={[10,10,-10]}/>
+            <pointLight intensity={1} position={[-10,10,-10]}/>
+            <primitive object ={gltf.scene} dispose={null} position={[0,0,0]}/>
+            <OrbitControls autoRotate={true} autoRotateSpeed={4} enableZoom={false} />  
+          </Canvas>
+        </div>
     </div>
   );
 }
